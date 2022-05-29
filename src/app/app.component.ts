79db6ad35954b61f7e7ad8ad099abcdec93b9c07
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-05-29 15:55:18 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ export class AppComponent  {
 
 class ordinePrenotazione { //per eliminare il problema della ripetizione delle strutture
   prenotazione = [];
-  value: string;
-  style: any;
+  value: any;
+  style: CSSStyleDeclaration;
   constructor(posti: any[][], elementName: HTMLInputElement) { //Il costruttore prende come parametri: l'array con i posti già prenotati e il nome dell'elemento HTML che ospita i bottoni
     //var element = document.getElementById(elementName);
     //crea i bottoni e le file di bottoni a ricreare il teatro che vogliamo costruire. Poi li inserisce nell'array prenotazione
