a7dca21a63736b6a5de8ce4973609e1ae6acc94e
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-05-29 16:15:25 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ class ordinePrenotazione { //per eliminare il problema della ripetizione delle s
   prenotazione = [];
   value: any;
   style: CSSStyleDeclaration;
-  constructor(posti: any[][], elementName: HTMLInputElement) { //Il costruttore prende come parametri: l'array con i posti già prenotati e il nome dell'elemento HTML che ospita i bottoni
+  constructor(posti: any[][], elementName: HTMLElement) { //Il costruttore prende come parametri: l'array con i posti già prenotati e il nome dell'elemento HTML che ospita i bottoni
     //var element = document.getElementById(elementName);
     //crea i bottoni e le file di bottoni a ricreare il teatro che vogliamo costruire. Poi li inserisce nell'array prenotazione
     this.prenotazione = posti.map((fila, i) => { 
@@ -53,8 +53,8 @@ class ordinePrenotazione { //per eliminare il problema della ripetizione delle s
 
 const prenotaEl = (document.getElementById('prenota') as HTMLInputElement);
 const nomeEl = (document.getElementById('nome') as HTMLInputElement);
-const nplatea = (document.getElementById('platea') as HTMLInputElement);
-const npalchi = (document.getElementById('palchi') as HTMLInputElement);
+const nplatea = document.getElementById('platea');
+const npalchi = document.getElementById('palchi');
 
 //costanti che definiscono la grandezza del teatro 
 const nfilePlatea = 7;
