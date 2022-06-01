bc2e1fee1fcfd18815da6166aa910e612088647c
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-01 12:56:41 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent  {
 
 const prenotaEl = (document.getElementById('prenota') as HTMLInputElement);
 const nomeEl = (document.getElementById('nome') as HTMLInputElement);
-const nplatea = (document.getElementById('platea') as HTMLElement);
-const npalchi = (document.getElementById('palchi') as HTMLElement);
+var nplatea = (document.getElementById('platea') as HTMLElement);
+var npalchi = (document.getElementById('palchi') as HTMLElement);
 
 //costanti che definiscono la grandezza del teatro 
 const nfilePlatea = 7;
