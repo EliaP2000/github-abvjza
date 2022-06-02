1abca6c001052c63c5d6135f122b4087d278c83e
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-02 11:49:11 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent  {
 
 const prenotaEl = (document.getElementById('prenota') as HTMLInputElement);
 const nomeEl = (document.getElementById('nome') as HTMLInputElement);
-const nplatea = (document.getElementById('platea') as HTMLElement);
-const npalchi = (document.getElementById('palchi') as HTMLElement);
+/*const nplatea = (document.getElementById('platea') as HTMLElement);
+const npalchi = (document.getElementById('palchi') as HTMLElement);*/
 
 //costanti che definiscono la grandezza del teatro 
 const nfilePlatea = 7;
@@ -33,13 +33,13 @@ class ordinePrenotazione { //per eliminare il problema della ripetizione delle s
   prenotazione = [];
   value: any;
   style: CSSStyleDeclaration;
-  constructor(posti: any[][], elementName: HTMLElement) { /*Il costruttore prende come parametri: l'array con i posti già prenotati e il nome dell'elemento HTML che ospita i bottoni
+  constructor(posti: any[][], elementName) { /*Il costruttore prende come parametri: l'array con i posti già prenotati e il nome dell'elemento HTML che ospita i bottoni*/
     var element = document.getElementById(elementName);
     //crea i bottoni e le file di bottoni a ricreare il teatro che vogliamo costruire. Poi li inserisce nell'array prenotazione*/
     this.prenotazione = posti.map((fila, i) => { 
       var p = fila.map((nome, j) => {
         var btn = document.createElement('button'); //crea i bottoni
-        elementName.appendChild(btn);
+        element.appendChild(btn);
         btn.value = nome;
         btn.style.color = (nome !== "x") ? 'red' : 'green'; //cambio di colore se contenente un nome oppure una x (posto non prenotato)
         btn.innerHTML = 'P' + (j + 1) + (i + 1); 
@@ -124,8 +124,8 @@ function mostraTeatro() { //mostra l'array risultante
  }
 
 //chiamata e inserimento nelle variabili della funzione OrdinePrenotazione per prenotare i posti in platea e sui palchi
-var plateaPrenotazione = new ordinePrenotazione(teatro.platea, nplatea);
-var palchiPrenotazione = new ordinePrenotazione(teatro.palchi, npalchi);
+var plateaPrenotazione = new ordinePrenotazione(teatro.platea, "platea");
+var palchiPrenotazione = new ordinePrenotazione(teatro.palchi, "palchi");
 
 //al click richiama la funzione mostraTeatro che richiama la funzione toArray e mostrando in console il teatro come un array di stringhe e non di pulsanti
 document.getElementById('Vedi');
